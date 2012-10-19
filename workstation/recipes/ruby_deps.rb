4a9dc75182c1ba71ca0131f6f1dbe9fc2369055d
Kortina <kortina@gmail.com>
2012-10-19 10:38:43 -0400

    add vagrant

--- workstation/recipes/ruby_deps.rb
+++ workstation/recipes/ruby_deps.rb
@@ -15,3 +15,10 @@
         not_if "gem list | grep \"^#{pkg} \""
     end
 end
+
+execute "make sure rvm creates vagrant executable in bin" do
+    user WS_USER
+    command "source $HOME/.rvm/scripts/rvm && rvm use ruby-1.9.3-p194"
+    action :run
+    not_if "which vagrant"
+end
