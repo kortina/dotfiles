db34c78626ab6000ec98da9e1efe49ed4e247d6b
Kortina <kortina@gmail.com>
2013-10-29 07:46:03 -0400

    removed cruft. will re-add if I ever need it

--- workstation/recipes/if_venmo.rb
+++
@@ -1,7 +0,0 @@
-dotfiles_path = File.expand_path(File.dirname(__FILE__)+"../../..") # a few levels up from this file
-
-if File.directory?("#{dotfiles_path}/../venmo-workstation")
-    include_recipe "venmo-workstation::all"
-else
-    puts "WARNING: looks like this is not a venmo-workstation. No venmo-workstation directory at same level as this dotfiles repo."
-end
