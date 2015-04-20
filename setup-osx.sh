f2dd998c7e25ce4f2064d4f04ea64bcc5934213a
Kortina <kortina@gmail.com>
2015-04-20 11:30:50 -0700

    faster os x animations

--- setup-osx.sh
+++ setup-osx.sh
@@ -9,5 +9,12 @@ defaults write com.apple.menuextra.clock DateFormat 'EEE MMM d  h:mm a'
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# some changes (eg, menubar) require this restart:
+# speed up Mission Control / slow animations
+# @see http://apple.stackexchange.com/questions/17929/how-can-i-disable-animation-when-switching-desktops-in-lion
+defaults write com.apple.dock expose-animation-duration -int 0
+defaults delete com.apple.dock expose-animation-duration
+
+# some changes (eg, menubar) require:
 killall SystemUIServer
+# some changes (eg, animations) require:
+killall Dock
