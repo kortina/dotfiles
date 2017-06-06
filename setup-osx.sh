75c69ef4d544ca1a437da0e52bb16917bae8a402
Kortina <kortina@gmail.com>
2017-06-05 19:14:30 -0700

    update osx config keyboard

--- setup-osx.sh
+++ setup-osx.sh
@@ -45,8 +45,11 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
+# Set a fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+# Set a fast delay until repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 ###############################################################################
 # Finder                                                                      #
