c1ab03ffd2783db6a413c383a9089950bab1750f
Kortina <kortina@gmail.com>
2017-11-08 09:03:33 -0800

    add fzf command ch to browse git history

--- .fzf.conf.bash
+++ .fzf.conf.bash
@@ -56,6 +56,27 @@ gr() {
     cut -d$'\t' -f1
 }
 
+
+########################################
+# from: https://junegunn.kr/2015/04/browsing-chrome-history-with-fzf
+########################################
+# see also: https://gist.github.com/junegunn/15859538658e449b886f - chrome bookmarks
+
+# ch - browse chrome history
+ch() {
+  local cols sep
+  cols=$(( COLUMNS / 3 ))
+  sep='{::}'
+
+  cp -f ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/h
+
+  sqlite3 -separator $sep /tmp/h \
+    "select substr(title, 1, $cols), url
+     from urls order by last_visit_time desc" |
+  awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
+  fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs open
+}
+
 ########################################
 # kortina
 ########################################
