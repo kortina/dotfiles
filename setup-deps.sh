d639f94131b0512991a1bbea84757316f89cbb9c
kortina <kortina@gmail.com>
2020-04-18 10:14:53 -0700

    add h2m

--- setup-deps.sh
+++ setup-deps.sh
@@ -193,6 +193,7 @@ npm_install eslint-plugin-react
 npm_install eslint-plugin-flowtype
 npm_install eslint-plugin-fin-eslint-flow-enforcement
 npm_install babel-eslint
+npm_install h2m # tool for downlaoding webpage as md file
 npm_install livedown
 npm_install prettier
 npm_install remark
