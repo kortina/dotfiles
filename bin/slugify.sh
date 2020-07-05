aa97aaedd6e04da0d1e498098e2096d66a95ff15
kortina <kortina@gmail.com>
2020-07-05 12:58:52 -0700

    fix slugify

--- bin/slugify.sh
+++ bin/slugify.sh
@@ -15,6 +15,6 @@ while getopts ":l" opt; do
     esac
 done
 
-cmd="cat - | tr '\n' '-' | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' $lowercase | sed 's/[- \n]*$//g'"
+cmd="cat - | tr '\n' '-' | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' $lowercase | sed 's/[- ]*$//g'"
 o=$(eval $cmd)
 echo "$o" | tr -d '\n'
