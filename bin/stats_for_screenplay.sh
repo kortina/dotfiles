866d98e234d7aa3991ee4b6cee3b7e51ac2c18f5
Kortina <kortina@gmail.com>
2021-12-27 11:51:33 -0500

    commify stats for screenplay

--- bin/stats_for_screenplay.sh
+++ bin/stats_for_screenplay.sh
@@ -9,23 +9,31 @@ exit 1;
 
 fi
 
+# guards
+
 # https://github.com/ifrost/afterwriting-labs/blob/master/docs/clients.md
 command -v afterwriting >/dev/null 2>&1 || { echo "ERROR: npm install -g afterwriting";  exit 1; }
 command -v qpdf >/dev/null 2>&1 || { echo "ERROR: brew install qpdf";  exit 1; }
+command -v rg >/dev/null 2>&1 || { echo "ERROR: brew install rg";  exit 1; }
 
-# vid, a screen reording, for example
+
+# files
 fp_screenplay=$1
 bn_screenplay=`basename "$1"`
 fp_pdf="/tmp/$bn_screenplay.pdf"
 
-# echo "afterwriting --overwrite --source \"$fp_screenplay\" --pdf \"$fp_pdf\" 2>&1 > /dev/null"
+# render a pdf in /tmp
 afterwriting --overwrite --source "$fp_screenplay" --pdf "$fp_pdf" 2>&1 > /dev/null
-
 
 
 words=`wc -w "$fp_screenplay" | tr -s ' ' | cut -d ' ' -f 2`
 lines=`wc -l "$fp_screenplay" | tr -s ' ' | cut -d ' ' -f 2`
 pages=`qpdf --show-npages "$fp_pdf"`
+
+words=`LC_NUMERIC=en_US.UTF-8 printf "%'d\n" "$words"`
+lines=`LC_NUMERIC=en_US.UTF-8 printf "%'d\n" "$lines"`
+pages=`LC_NUMERIC=en_US.UTF-8 printf "%'d\n" "$pages"`
+
 echo "-------------------"
 echo "File:  $bn_screenplay"
 echo "Words: $words"
