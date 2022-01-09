94c6379f91734ac46343f23f096b544d3c92293a
Kortina <kortina@gmail.com>
2022-01-09 13:26:12 -0500

    fix dl-markdown script

--- bin/dl-markdown.sh
+++ bin/dl-markdown.sh
@@ -3,8 +3,19 @@
 # deps:
 # ripgrep
 # readability @ 0.1.0
+# npm install readability-cli
 # html2md @ 0.4.4
 # X h2m @ 0.7.0
+# brew install pandoc
+
+command_guard()
+{
+    echo "check: $1"
+    if ! command -v "$1" &> /dev/null ; then
+        echo "$1 NOT FOUND";
+        exit;
+    fi
+}
 
 _dl_markdown() {
     url="$1"
@@ -15,15 +26,32 @@ _dl_markdown() {
     readable="$fn.readable.html"
     md="$fn.md"
     curl -s "$url" > "$raw" 2>/dev/null
-    readability "$raw" >"$readable" 2>/dev/null
-    html2md -g -i "$readable" > "$md"
+    command_guard "readable"
+    readable "$raw" >"$readable" # 2>/dev/null
+
+    echo "$raw"
+    echo "$readable"
+
+    # html2md -g -i "$readable" > "$md"
+    command_guard "pandoc"
+    # markdown_type="markdown"
+    markdown_type="commonmark" # https://pandoc.org/MANUAL.html
+    pandoc -f html -t "$markdown_type" "$readable" -o "$md"
+    # now, strip all the native html (div, etc) elements
+    # via: https://stackoverflow.com/questions/53311148/remove-html-elements-inside-markdown
+    pandoc --from=markdown --to=html "$md" | \
+    pandoc --from=html --to="$markdown_type-raw_html" --output "$md"
+
     # extract title from raw html and slugify
     title="`rg --no-line-number -o '<title>(.*)</title>' $raw --replace '$1' | head -1 |  tr '\n' '-' | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' $lowercase | sed 's/[- \n]*$//g'`"
     # if we did not find a title, just use fbase
     [ -z "$title" ] && title="$fbase"
+    # truncate to 128 chars
+    title=`echo "$title" | cut -c1-128`
     # either way, add markdown ext
     title_md="$title.md"
     cp "$md" "$title_md"
+    echo -e "\n-- via <$url>" >> "$title_md"
     echo -e "Markdown at:\n$title_md"
 }
 
