2635793b339b620b3ae48c1aac963ab1a20510e7
Kortina <kortina@gmail.com>
2022-07-08 17:40:36 -0400

    add types-pytz

--- setup-deps.sh
+++ setup-deps.sh
@@ -113,6 +113,7 @@ pip_install pytz
 pip_install python-magic
 pip_install screenplain
 pip_install vimeo_downloader
+pip_install types-pytz
 pip_install watchdog
 pip_install xlsx2csv
 pip install -U yt-dlp
