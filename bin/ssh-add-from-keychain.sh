6e13b8772917ca2c7fbcde2304cdcd5dd94401a0
kortina <kortina@gmail.com>
2022-08-03 13:23:21 -0400

    update add from keychain

--- bin/ssh-add-from-keychain.sh
+++ bin/ssh-add-from-keychain.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # script to ssh-add primary ssh key using passphrase stored in mac keychain
-ssh-add -l | grep '.ssh/id_rsa ' > /dev/null || ssh-add -K
+ssh-add -l | grep '.ssh/id_rsa ' > /dev/null || ssh-add --apple-use-keychain
