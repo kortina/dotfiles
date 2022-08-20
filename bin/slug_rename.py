ef15287fcf8f3ed1e649d71452725f8487931610
Kortina <kortina@gmail.com>
2022-08-20 11:58:15 -0400

    slug_rename service

---
+++ bin/slug_rename.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import re
+import shutil
+import subprocess
+
+
+class Settings:
+    debug = False
+
+
+def run_cmd(cmd_args: list):
+    cmd = " ".join(cmd_args)
+    if Settings.debug:
+        print(f"running:\n{cmd}")
+    subprocess.run(cmd_args, check=True)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="""Slugify rename a file (replace all special chars with hyphens) """
+    )
+    parser.add_argument(
+        "--debug",
+        dest="debug",
+        action="store_true",
+        help="Set debug mode output.",
+    )
+    parser.add_argument(
+        "file",
+        type=str,
+        help="file to rename",
+        default=None,
+    )
+    args = parser.parse_args()
+    orig = args.file
+
+    if args.debug:
+        Settings.debug = True
+
+    if not os.path.isfile(orig):
+        parser.error(f"file '{args.file}' does not exist.")
+
+    orig_base = os.path.basename(orig)
+    orig_abs = os.path.abspath(orig)
+    orig_abs_dir = os.path.dirname(orig_abs)
+    orig_name, orig_ext = os.path.splitext(orig_base)
+
+    print(orig_base)
+    print(orig_abs)
+    print(orig_abs_dir)
+    print(orig_name)
+    print(orig_ext)
+
+    slug_name = re.sub(r"[^\w]+", "-", orig_name)
+    print(slug_name)
+    new_name = f"{slug_name}{orig_ext}"
+    print(new_name)
+    new_abs = os.path.join(orig_abs_dir, new_name)
+    print(new_abs)
+
+    print(f'mv "{orig_abs}" "{new_abs}"')
+    shutil.move(orig_abs, new_abs)
+
+
+if __name__ == "__main__":
+    main()
