f094b403ade35827897a31665107576e328d7b02
Kortina <kortina@gmail.com>
2022-11-29 12:10:45 -0500

    fix unsub

--- js/bookmarklet.gmail.unsub.js
+++ js/bookmarklet.gmail.unsub.js
@@ -1,9 +1,32 @@
 // javascript:
-Array.from(document.getElementsByTagName('span'))
-  .filter((e) => e.getAttribute('role') == 'link' && e.innerText == 'Unsubscribe')
-  .map((e) => e.click());
-setTimeout(() => {
-  Array.from(document.getElementsByTagName('button'))
-    .filter((e) => e.innerText == 'Unsubscribe')
-    .map((e) => e.click());
-}, 100);
+(function () {
+  window.uMatches = (nodeName, pattern) => {
+    return Array.from(document.getElementsByTagName(nodeName)).filter(
+      (e) => e.innerText && e.innerText.match(pattern)
+    );
+  };
+  window.uNodes = Array.from(document.getElementsByTagName('span')).filter(
+    (e) => e.getAttribute('role') == 'link' && e.innerText == 'Unsubscribe'
+  );
+  if (window.uNodes.length) {
+    uNodes.map((e) => {
+      e.click();
+      setTimeout(() => {
+        window.uMatches('button', /^Unsubscribe$/).map((e) => e.click());
+      }, 100);
+    });
+  }
+  if (!window.uNodes.length) {
+    window.uNodes = window.uMatches('a', /unsub|optout|opt out|opt-out/i);
+  }
+  if (!window.uNodes.length) {
+    window.uNodes = window.uMatches('a', /click here|clickhere/i);
+  }
+  if (!window.uNodes.length) {
+    window.uNodes = window.uMatches('a', /here/i);
+  }
+  if (window.uNodes.length) {
+    window.uNodes.reverse();
+    window.uNodes[0].click();
+  }
+})();
