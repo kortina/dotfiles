331ac418e332597fbb680efcd44671769e98ebb7
Kortina <kortina@gmail.com>
2023-02-01 15:10:41 -0500

    add util to list all objects in bucket

---
+++ bin/s3_list_all_objects.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# List all the files in a bucket.
+#
+# Configuration
+#
+# export CYBERDUCK_AWS_ID='__your_id__'
+# export CYBERDUCK_AWS_SECRET='__your_secret__'
+#
+
+
+import argparse
+import boto3
+import os
+
+
+def _get_required_env_var(var):
+    val = os.environ.get(var)
+    if val in [None, ""]:
+        raise NameError(f"Required env var {var} was not set.")
+    return val
+
+
+def get_all_s3_objects(s3_client, **base_kwargs):
+    continuation_token = None
+    while True:
+        list_kwargs = dict(MaxKeys=1000, **base_kwargs)
+        if continuation_token:
+            list_kwargs["ContinuationToken"] = continuation_token
+        response = s3_client.list_objects_v2(**list_kwargs)
+        yield from response.get("Contents", [])
+        if not response.get("IsTruncated"):  # At the end of the list?
+            break
+        continuation_token = response.get("NextContinuationToken")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("python s3_list_all_objects.py")
+    parser.add_argument(
+        "bucket",
+        type=str,
+        help="Bucket to list objects from.",
+    )
+    parser.add_argument(
+        "--prefix",
+        default=None,
+        type=str,
+        help="Prefix to match.",
+    )
+    args = parser.parse_args()
+    bucket = args.bucket
+    prefix = args.prefix or ""
+
+    aws_id = _get_required_env_var("CYBERDUCK_AWS_ID")
+    aws_secret = _get_required_env_var("CYBERDUCK_AWS_SECRET")
+    s3_client = boto3.client(
+        "s3", aws_access_key_id=aws_id, aws_secret_access_key=aws_secret
+    )
+
+    for file in get_all_s3_objects(s3_client, Bucket=bucket, Prefix=prefix):
+        print(file["Key"])
