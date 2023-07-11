ea7f0241506eec6eba0705205eab0703be10b2cd
Kortina <kortina@gmail.com>
2023-07-11 16:51:12 -0400

    make heic script case-insensitive

--- bin/x_heic_2_jpg.sh
+++ bin/x_heic_2_jpg.sh
@@ -5,22 +5,19 @@
 # 1
 set -eu -o pipefail
 
-# allow passing of a different extension (eg, HEIC) as arg1:
-ARG1=${1:-heic}
-
-ext="$ARG1"
 # 2
-count=$(find . -depth 1 -name "*.$ext" | wc -l | sed 's/[[:space:]]*//')
-echo "converting $count files .$ext files to .jpg"
+count=$(find . -depth 1 -iname "*.heic" | wc -l | sed 's/[[:space:]]*//')
+echo "converting $count files .heic or .HEIC files to .jpg"
 
 # 3
-magick mogrify -monitor -format jpg *.$ext
+magick mogrify -monitor -format jpg *.heic
+magick mogrify -monitor -format jpg *.HEIC
 
 # 4
-echo "Remove .$ext files? [y/n]"
+echo "Remove .heic / .HEIC files? [y/n]"
 read remove
 
 # 5
 if [[ "$remove" == "y" ]]; then
-  find . -depth 1 -name "*.$ext" -delete
+  find . -depth 1 -iname "*.heic" -delete
 fi
