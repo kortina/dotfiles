b34d87ab9ba94f32f2b89ac1a78bf0dd90d47f0e
Kortina <kortina@gmail.com>
2024-01-08 14:49:02 -0500

    parse date and timzezone offset. store utc instead of local time in db

--- bin/gmail_scrape_contacts/gmail_scrape_contacts.py
+++ bin/gmail_scrape_contacts/gmail_scrape_contacts.py
@@ -15,10 +15,12 @@ import email.utils
 import pprint
 import re
 from dataclasses import dataclass, field
-from datetime import datetime
+from datetime import datetime, timedelta
 from email.header import decode_header
+from email.utils import mktime_tz, parsedate_tz
 from typing import Any, List, Optional
 
+import pytz
 from dateutil import parser
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
@@ -48,6 +50,31 @@ def pp(obj):
 
 def parse_dt(date_str):
     return parser.parse(date_str)
+
+
+# parse a date string into
+# utc datetime and tz offset string ("+0100", "-0800")
+def parse_dtz(date_str: str):
+    # parse the string to datetime object
+    parsed_date = parsedate_tz(date_str)
+
+    # ensure this is a 10 element tuple
+    if parsed_date:
+        timestamp = mktime_tz(parsed_date)
+    else:
+        raise Exception(f"ak: PARSE DATE FAILED {date_str}")
+    date_in_utc = datetime.fromtimestamp(timestamp, pytz.utc)
+
+    # extract the timezone offset
+    tz_offset_seconds = float(parsed_date[9] or 0)
+    tz_offset = timedelta(seconds=tz_offset_seconds)
+
+    # format the offset as a string (e.g., +0100, -0800)
+    offset_hours = int(tz_offset.total_seconds() // 3600)
+    offset_minutes = int((tz_offset.total_seconds() % 3600) // 60)
+    tz_str = f"{offset_hours:+03d}{offset_minutes:02d}"
+
+    return date_in_utc, tz_str
 
 
 def default_dt():
@@ -128,6 +155,7 @@ class Email(Base):
     subject: Mapped[str] = mapped_column(String, nullable=True)
     c_from: Mapped[str] = mapped_column(String, default=UNDEF)
     date: Mapped[datetime] = mapped_column(DateTime, default=default_dt)
+    tz: Mapped[str] = mapped_column(String, default=UNDEF)
     date_raw: Mapped[str] = mapped_column(String, default=UNDEF)
     headers_raw: Mapped[str] = mapped_column(String, default="{}")
 
@@ -152,7 +180,10 @@ class Email(Base):
         # get subject, date, from from headers
         e.subject = str(e.headers.get("Subject") or UNDEF)
         e.date_raw = str(e.headers.get("Date") or UNDEF)
-        e.date = parse_dt(e.date_raw)
+        dt, tz = parse_dtz(e.date_raw)
+        e.date = dt
+        e.tz = tz
+        # e.date = parse_dt(e.date_raw)
 
         _from = e.headers.get(cls.FROM)
         if _from:
@@ -160,6 +191,25 @@ class Email(Base):
             if contacts:
                 e.c_from = contacts[0].email
         return e
+
+    @classmethod
+    def oldest(cls):
+        oldest = db_session.query(cls).order_by(cls.date.asc()).first()
+        print(oldest)
+        return oldest
+
+    @classmethod
+    def oldest_timestamp(cls):
+        # format date of oldest email as
+        #  All dates used in the search query are interpreted as midnight on
+        #    that date in the PST timezone. To specify accurate dates for other
+        #    timezones pass the value in seconds instead:
+        #
+        # ?q=in:sent after:1388552400 before:1391230800
+        # https://developers.google.com/gmail/api/guides/filtering
+        oldest = cls.oldest()
+        if oldest:
+            breakpoint()
 
 
 @dataclass
@@ -171,8 +221,8 @@ class EmailContact(Base):
     email_id: Mapped[str] = mapped_column(
         String, ForeignKey("emails.id"), index=True, default=UNDEF
     )
+    # could be useful for quick aggregations:
     thread_id: Mapped[str] = mapped_column(String, default=UNDEF)
-    subject: Mapped[str] = mapped_column(String, default=UNDEF)
     name: Mapped[str] = mapped_column(String, default=UNDEF)
     email: Mapped[str] = mapped_column(
         String, default=UNDEF
@@ -180,13 +230,16 @@ class EmailContact(Base):
     email_raw: Mapped[str] = mapped_column(String, default=UNDEF)
 
     date: Mapped[datetime] = mapped_column(DateTime, default=default_dt)
-    date_raw: Mapped[str] = mapped_column(String, default=UNDEF)
     header: Mapped[str] = mapped_column(String, default=UNDEF)
     # don't confuse this for a database column:
     _contact: Optional[Contact] = None
 
     def __repr__(self):
-        return f"<{self.__class__.__name__} {self.header}: {self.email} // email_subject: {self.subject}>"
+        # date in YYYY-MM-DD format:
+        dt = "None"
+        if self.date:
+            dt = self.date.strftime("%Y-%m-%d")
+        return f"<{self.__class__.__name__} {self.header}: {self.email} // {dt}>"
 
     @classmethod
     def from_email(cls, email) -> List["EmailContact"]:
@@ -202,12 +255,10 @@ class EmailContact(Base):
                         ec._contact = c
                         ec.email_id = email.id
                         ec.thread_id = email.thread_id
-                        ec.subject = email.subject
                         ec.name = c.name
                         ec.email = c.email
                         ec.email_raw = c.email_raw
                         ec.date = email.date
-                        ec.date_raw = email.date_raw
                         ec.header = _h_name
                         print(ec)
                         ecs.append(ec)
@@ -249,8 +300,12 @@ def fetch_messages(service, messages):
     return email
 
 
-def scrape():
+def scrape(resume_oldest: bool):
     create_tables_if_not_exist()
+    # search query for the gmail api:
+    q = ""
+    if resume_oldest:
+        oldest_timestamp = Email.oldest_timestamp()
 
     credentials = Credentials.from_authorized_user_file(CREDENTIALS_PATH)
 
@@ -434,11 +489,12 @@ def rank():
 def main():
     parser = argparse.ArgumentParser(description="""Download and rank google contacts.""")
     parser.add_argument("action", help="action to take", choices=["scrape", "rank"])
+    parser.add_argument("--resume-oldest", action="store_true")
     # parser.add_argument("-v", "--verbose", action="store_true")
     args = parser.parse_args()
 
     if args.action == "scrape":
-        scrape()
+        scrape(resume_oldest=args.resume_oldest)
     elif args.action == "rank":
         rank()
     else:
--- bin/gmail_scrape_contacts/gmail_scrape_contacts.sql
+++ bin/gmail_scrape_contacts/gmail_scrape_contacts.sql
@@ -9,3 +9,15 @@ ORDER BY
     `date` desc
 LIMIT
     100;
+
+----------------------------------------
+-- oldest email date
+----------------------------------------
+SELECT
+    *
+FROM
+    emails
+ORDER BY
+    `date` asc
+LIMIT
+    1;
