1c419db0b005f8c9c672a77f3d2e9d3cc7f33d45
Kortina <kortina@gmail.com>
2024-01-08 22:09:37 -0500

    write some ranking queries

--- bin/gmail_scrape_contacts/gmail_scrape_contacts.sql
+++ bin/gmail_scrape_contacts/gmail_scrape_contacts.sql
@@ -21,3 +21,24 @@ ORDER BY
     `date` asc
 LIMIT
     1;
+
+----------------------------------------
+-- most popular senders
+----------------------------------------
+SELECT
+    name,
+    email,
+    header,
+    COUNT(DISTINCT (thread_id)) AS c
+FROM
+    email_contacts
+WHERE
+    header = 'From'
+    AND email NOT LIKE '%reply%'
+    AND email NOT LIKE '%info%'
+    AND email NOT REGEXP '4pfp|accounts*@|admin|agent@|alert|amazon|assistant\.|coned|customer|document|docusign|email@|events*@|filmfest@|festival@|googlegroups|googlenest|hello@|help|invoice|lexisnexis|marketing|momence|mskcc|my_merrill|notifications*@|notifier|orders*@|proxyvote\.com|quotes*@|robot@|security|securemessag|service|statement|submissions*@|subscription|substack|support|sxsw@|team@|ticket|update|venmo@|verizonwireless|vimeo@|welcome'
+GROUP BY
+    name,
+    email
+ORDER BY
+    c desc;
